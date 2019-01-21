31d3526c17931a5aa0080aa44657f4fa2804058c
Paul Tobin <tobster111@gmail.com>
2019-01-21 11:10:36 +0000

    Added User + UserInput to graphql schema

--- app.js
+++ app.js
@@ -30,6 +30,12 @@ app.use(
         avgRating: Float
       }
 
+      type User {
+        _id: ID!
+        email: String!
+        password: String
+      }
+
       input ReplayInput {
         title: String!
         team1: String!
@@ -43,6 +49,11 @@ app.use(
         downloads: Float
         winner: String
         avgRating: Float
+      }
+
+      input UserInput {
+        email: String!
+        password: String!
       }
 
       type RootQuery {
