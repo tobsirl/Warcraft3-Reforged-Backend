5ac7c389cfa15af6690efb2a226fb359700bde79
Paul Tobin <tobster111@gmail.com>
2019-01-23 10:50:55 +0000

    rename functions due to linter + added new linting rule

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,27 +3,28 @@ const bcrypt = require('bcryptjs');
 const Replay = require('../../models/replay');
 const User = require('../../models/user');
 
-const replays = async replayIds => {
+const replaysFetch = async replayIds => {
   try {
     const replays = await Replay.find({ _id: { $in: replayIds } });
     replays.map(replay => ({
       ...replay._doc,
       _id: replay.id,
       releaseDate: new Date(replay._doc.releaseDate).toISOString(),
-      submitter: user.bind(this, replay.submitter)
+      submitter: userFetch.bind(this, replay.submitter)
     }));
+    return replays;
   } catch (err) {
     throw err;
   }
 };
 
-const user = async userId => {
+const userFetch = async userId => {
   try {
     const user = await User.findById(userId);
     return {
       ...user._doc,
       id: user.id,
-      submittedReplay: replays.bind(this, user._doc.submittedReplay)
+      submittedReplay: replaysFetch.bind(this, user._doc.submittedReplay)
     };
   } catch (err) {
     throw err;
@@ -34,10 +35,10 @@ module.exports = {
   replays: async () => {
     try {
       const replays = await Replay.find();
-      replays.map(replay => ({
+      return replays.map(replay => ({
         ...replay._doc,
         releaseDate: new Date(replay._doc.releaseDate).toISOString(),
-        submitter: user.bind(this, replay._doc.submitter)
+        submitter: userFetch.bind(this, replay._doc.submitter)
       }));
     } catch (err) {
       throw err;
@@ -58,7 +59,7 @@ module.exports = {
       downloads: args.replayInput.downloads,
       winner: args.replayInput.winner,
       avgRating: args.replayInput.avgRating,
-      submitter: '5c45be68faa221039006adc2'
+      submitter: '5c470d34dd5f5723888e306b'
     });
     let createdReplay;
 
@@ -68,34 +69,35 @@ module.exports = {
         ...result._doc,
         _id: result._doc._id.toString(),
         releaseDate: new Date(replay._doc.releaseDate).toISOString(),
-        submitter: user.bind(this, result._doc.submitter)
+        submitter: userFetch.bind(this, result._doc.submitter)
       };
-      const user = await User.findById('5c45be68faa221039006adc2');
+      const userdata = await User.findById('5c470d34dd5f5723888e306b');
 
-      if (!user) {
+      if (!userdata) {
         throw new Error('User not found.');
       }
-      user.submittedReplay.push(replay);
-      await user.save();
+      userdata.submittedReplay.push(replay);
+      await userdata.save();
       return createdReplay;
     } catch (err) {
       throw err;
     }
   },
+
   createUser: async args => {
     try {
-      const user = await User.findOne({ email: args.userInput.email });
+      const existingUser = await User.findOne({ email: args.userInput.email });
 
-      if (user) {
+      if (existingUser) {
         throw new Error('User exists already.');
       }
       const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
 
-      const user = new User({
+      const userData = new User({
         email: args.userInput.email,
         password: hashedPassword
       });
-      const result = await user.save();
+      const result = await userData.save();
       return { ...result._doc, password: null, _id: result.id };
     } catch (err) {
       throw err;
