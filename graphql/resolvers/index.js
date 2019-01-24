1ac3e59a41f3f9f95a3e155b0595728f526fd1ca
Paul Tobin <tobster111@gmail.com>
2019-01-24 12:47:21 +0000

    Refactoring finished

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,2 +1,11 @@
+const authResolver = require('./auth');
+const replayResolver = require('./replays');
+const uploadResolver = require('./uploads');
 
+const rootResolver = {
+  ...authResolver,
+  ...replayResolver,
+  ...uploadResolver
+};
 
+module.exports = rootResolver;
