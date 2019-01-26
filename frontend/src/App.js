79cd0fbf7df38d6232ac660387528fd41fb3d2d1
Paul Tobin <tobster111@gmail.com>
2019-01-26 11:51:38 +0000

    Added Pages + Auth, Routing to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>It works!</h1>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect path="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/replays" component={null} />
+          <Route path="/uploads" component={null} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
