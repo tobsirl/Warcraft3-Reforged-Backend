46f58ff2d433f610555b70112eedde544001d3d7
Paul Tobin <tobster111@gmail.com>
2019-01-26 11:53:26 +0000

    Added Replays + Uploads components

---
+++ frontend/src/pages/Replays.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Replays extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Replays page</h1>
+      </div>
+    );
+  }
+}
+
+export default Replays;
---
+++ frontend/src/pages/Uploads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Uploads extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Uploads Page</h1>
+      </div>
+    );
+  }
+}
+
+export default Uploads;
