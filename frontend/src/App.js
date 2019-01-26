c65053c56c97551c115e46c7a9af89f0b2eeaacb
Paul Tobin <tobster111@gmail.com>
2019-01-26 11:02:59 +0000

    Setup eslint

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <h1>It works!</h1>
       </div>
     );
   }
